98dd2447f600e3981e25d7b55967526a3b4e05d6
srilekab2024laiml-lang <srileka.b2024laiml@sece.ac.in>
2025-12-19 10:07:23 +0530

    Remove host parameter from app.listen

--- backend/server.js
+++ backend/server.js
@@ -2,6 +2,6 @@ const app = require("./app");
 
 const PORT = process.env.PORT || 5000;
 
-app.listen(PORT,'0.0.0.0',() => {
+app.listen(PORT,() => {
   console.log("Server running on port", PORT);
 });
